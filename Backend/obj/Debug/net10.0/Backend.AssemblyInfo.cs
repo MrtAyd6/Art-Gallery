da59e6a739c333a69f7bf1a76645bdc35e49e58c
MrtAyd6 <mrmurat5757@gmail.com>
2026-04-20 16:13:11 +0300

    Rezervasyon iptal endpoint eklendi.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734f800bf283f256f3d319743af4b9519436cbd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9bc03d67e4ced5702a405eca9a5f5d78ab5d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
