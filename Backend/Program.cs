d5379f74b5be127f1d8674df9cb44b4131c20c07
MrtAyd6 <mrmurat5757@gmail.com>
2026-04-21 22:53:09 +0300

    login ve register sayfa tasarımları ve fonksiyonelliği tamamlandı.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,7 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 //Controller servislerini sisteme dahil eder
 builder.Services.AddControllers();
 
+//Frontendden gelen isteklere izin ver (CORS)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 //Gelen HTTP isteklerinin controllera haritalar
 app.MapControllers();
