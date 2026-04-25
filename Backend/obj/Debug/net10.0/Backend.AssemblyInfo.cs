62a90caec2f3157a9e06997e813670af841c4914
MrtAyd6 <mrmurat5757@gmail.com>
2026-04-25 21:03:04 +0300

    Yorum yapma endpointleri ve veritabanı eklendi

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da59e6a739c333a69f7bf1a76645bdc35e49e58c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a27c4aa75c017ea128e68e163d8d3f97f561dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
